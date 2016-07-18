9f3dac67ed727c18226bf01d6e366a0c841be962
Felix Duvallet <felix.duvallet@epfl.ch>
2016-07-18 15:41:09 +0200

    pep8

--- ros_boilerplate/src_python/ros_boilerplate/boilerplate_node.py
+++ ros_boilerplate/src_python/ros_boilerplate/boilerplate_node.py
@@ -2,13 +2,13 @@
 
 import rospy
 
+
 def run():
     rospy.init_node('boilerplate')
 
     rospy.loginfo('Node started')
 
     rospy.sleep(0.5)
-
 
     rospy.loginfo('Running until shutdown (Ctrl-C).')
     while not rospy.is_shutdown():
