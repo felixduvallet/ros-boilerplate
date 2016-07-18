7787942d63f3655e854b20532f6cdf478521ed6e
Felix Duvallet <felix.duvallet@epfl.ch>
2016-07-18 15:57:41 +0200

    linting.

--- ros_boilerplate/src_python/ros_boilerplate/boilerplate_class.py
+++ ros_boilerplate/src_python/ros_boilerplate/boilerplate_class.py
@@ -3,17 +3,24 @@ from std_msgs.msg import String
 
 
 class BoilerPlate(object):
+    """
+    This class just talks to itself.
+    """
+
     def __init__(self):
         self._pub = rospy.Publisher('/foo', String, queue_size=10)
-        self._sub = rospy.Subscriber('/foo', String, self.string_cb)
-        self._x = 0
+        self._sub = rospy.Subscriber('/foo', String, self._string_cb)
+        self._count = 0
 
-
-    def string_cb(self, msg):
+    def _string_cb(self, msg):
+        """
+        Callback: just prints what it heard.
+        """
         rospy.loginfo('Received {}'.format(msg.data))
-        pass
 
     def run_once(self):
-        self._x += 1
-        self._pub.publish('Hello world #{}'.format(self._x))
-        pass
+        """
+        Main doer method. Blocking here is generally bad.
+        """
+        self._count += 1
+        self._pub.publish('Hello world #{}'.format(self._count))
