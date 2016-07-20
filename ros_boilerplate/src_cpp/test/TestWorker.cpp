3fc1ba3e61eeea9c14925f835caa4c924f78ee5c
Felix Duvallet <felix.duvallet@epfl.ch>
2016-07-20 10:22:57 +0200

    Add test code.

---
+++ ros_boilerplate/src_cpp/test/TestWorker.cpp
@@ -0,0 +1,27 @@
+#include <gtest/gtest.h>
+#include <memory>
+#include <ros_boilerplate/BasicWorker.h>
+
+namespace {
+
+
+class BasicTest: public ::testing::Test {
+
+  void SetUp() {
+    worker.reset(new BasicWorker(NULL));
+  }
+
+ public:
+  std::unique_ptr<BasicWorker> worker;
+};
+
+TEST_F(BasicTest, TestSomething) {
+  ASSERT_EQ(true, true);
+}
+
+}  // namespace
+
+int main(int argc, char **argv) {
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
